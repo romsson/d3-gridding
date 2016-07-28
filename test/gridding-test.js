28abc2d58babee1ca98409a50d185678cb92fdac
romsson <romain.vuillemot@gmail.com>
2016-07-28 15:31:56 +0200

    Tests for size and identity

--- test/gridding-test.js
+++ test/gridding-test.js
@@ -1,7 +1,30 @@
 var tape = require("tape"),
     gridding = require("../");
 
-tape("gridding() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-  test.equal(gridding.gridding(), 42);
+tape("gridding() is a function", function(test) {
+  test.equal(typeof gridding.gridding, 'function');
+  test.end();
+});
+
+tape("default size", function(test) {
+  test.deepEqual(gridding.gridding().size(), [1, 1]);
+  test.end();
+});
+
+tape("changing size", function(test) {
+  test.deepEqual(gridding.gridding().size([800, 600]).size(), [800, 600]);
+  test.end();
+});
+
+tape("identity function returns the very same grid positions", function(test) {
+
+  var nodes = [
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+    {x: 1, y: 0}
+  ];
+
+  test.deepEqual(nodes, gridding.gridding()(nodes), gridding);
   test.end();
 });
