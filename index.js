e2c85d5aa7211aed42b61509820a74da349a2f50
romsson <romain.vuillemot@gmail.com>
2016-07-28 15:16:07 +0200

    Adding size and d3 scale

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+import * as d3_scale from 'd3-scale';
+
 export {default as gridding} from "./src/gridding";
--- src/gridding.js
+++ src/gridding.js
@@ -1,6 +1,9 @@
+import {ordinal} from "d3-scale";
+
 export default function() {
   var mode = "identity",
-      layout = identity;
+      layout = identity,
+      size = [1, 1];
 
   function gridding(nodes) {
     return layout(nodes);
@@ -8,6 +11,12 @@ export default function() {
 
   function identity(nodes) {
     return nodes;
+  }
+
+  gridding.size = function(value) {
+    if (!arguments.length) return size;
+    size = value;
+    return gridding;
   }
 
   return gridding;
