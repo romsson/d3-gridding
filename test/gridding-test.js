96ddbfd603519f5050ba2d55c1690bc3ef3b5aa5
romsson <romain.vuillemot@gmail.com>
2016-08-02 11:17:01 +0200

    Adding tests

--- test/gridding-test.js
+++ test/gridding-test.js
@@ -17,7 +17,15 @@ tape("changing size", function(test) {
 });
 
 tape(".value() returns same value by default", function(test) {
-  test.deepEqual(gridding.gridding().value()(1), 1);
+  test.equal(gridding.gridding().value()(1), 1);
+  test.end();
+});
+
+tape(".value() custom value accessor should return the right value", function(test) {
+  var accessor_value = function(d) { return d.index; };
+  var nodes = [{index: 0, x: 0, y: 0}, {index: 1, x: 1, y: 0}];
+  var grid = gridding.gridding().size([10, 20]).value(accessor_value);
+  test.equal(grid.value()(nodes[1]), 1);
   test.end();
 });
 
@@ -31,5 +39,39 @@ tape("identity layout returns the very same grid positions", function(test) {
   ];
 
   test.deepEqual(nodes, gridding.gridding()(nodes));
+  test.end();
+});
+
+tape("horizontal layout should return 0s for Xs, and height/n for Ys", function(test) {
+
+  var nodes = [
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+    {x: 1, y: 0}
+  ];
+
+  var grid = gridding.gridding().mode("horizontal").size([10, 20]);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [0, 0, 0, 0]);
+  test.deepEqual(nodes.map(function(d) { return d.y; }), [0, 5, 10, 15]);
+  test.end();
+});
+
+tape("vertical layout should return width/n for Xs, and 0s for Ys", function(test) {
+
+  var nodes = [
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+    {x: 1, y: 0}
+  ];
+
+  var grid = gridding.gridding().mode("vertical").size([20, 10]);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [0, 5, 10, 15]);
+  test.deepEqual(nodes.map(function(d) { return d.y; }), [0, 0, 0, 0]);
   test.end();
 });
