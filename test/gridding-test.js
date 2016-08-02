1d26e3ea80981bfc15bc3ace9d6d268fec0c03a1
romsson <romain.vuillemot@gmail.com>
2016-08-02 14:16:42 +0200

    Tests for sort

--- test/gridding-test.js
+++ test/gridding-test.js
@@ -75,3 +75,45 @@ tape("vertical layout should return width/n for Xs, and 0s for Ys", function(tes
   test.deepEqual(nodes.map(function(d) { return d.y; }), [0, 0, 0, 0]);
   test.end();
 });
+
+tape("sort is ascending by default", function(test) {
+
+  var nodes = [
+    {x: 0, y: 0, index: 3},
+    {x: 0, y: 1, index: 2},
+    {x: 1, y: 1, index: 1},
+    {x: 1, y: 0, index: 0}
+  ];
+
+  var grid = gridding.gridding().mode("vertical").size([20, 10])
+                      .value(function(d) { return d.index; });
+
+  grid(nodes);
+
+  test.equal(nodes[0].index, 0);
+  test.equal(grid.value()(nodes[0]), 0);
+
+  test.end();
+});
+
+tape("sort now by descending value", function(test) {
+
+  var nodes = [
+    {x: 0, y: 0, index: 3},
+    {x: 0, y: 1, index: 2},
+    {x: 1, y: 1, index: 1},
+    {x: 1, y: 0, index: 0}
+  ];
+
+  var grid = gridding.gridding().mode("vertical").size([20, 10])
+                      .value(function(d) { return d.index; })
+                      .sort(function(a, b) { return b - a; });
+
+  grid(nodes);
+
+  test.equal(nodes[0].index, 3);
+  test.equal(grid.value()(nodes[0]), 3);
+
+  test.end();
+});
+
