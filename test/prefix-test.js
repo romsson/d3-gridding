f89aeffdcfce077396248d6c5fbc4a141108029f
romsson <romain.vuillemot@gmail.com>
2017-01-16 10:51:02 +0100

    More prefix tests

--- test/prefix-test.js
+++ test/prefix-test.js
@@ -17,5 +17,26 @@ tape(".prefix('_') has changed and should return the right prefix value", functi
   var grid = gridding.gridding().prefix("__").mode("grid");
   test.equal(typeof grid([1, 2, 3])[0]["x"], "undefined");
   test.equal(typeof grid([1, 2, 3])[0]["__x"], "number");
+
+  var grid = gridding.gridding().prefix("").mode("grid");
+  test.equal(typeof grid([1, 2, 3])[0]["x"], "number");
+  test.equal(typeof grid([1, 2, 3])[0]["__x"], "undefined");
+
+  var grid = gridding.gridding().prefix("_").mode("grid");
+  test.equal(typeof grid([1, 2, 3])[0]["x"], "undefined");
+  test.equal(typeof grid([1, 2, 3])[0]["__x"], "undefined");
+  test.equal(typeof grid([1, 2, 3])[0]["_x"], "number");
+
   test.end();
+});
+
+tape(".prefix() should work with all modes", function(test) {
+  var grid = gridding.gridding().prefix("");
+
+  grid.modes().forEach(function(mode) {
+    grid.mode(mode);
+    test.equal(typeof grid([1, 2, 3])[0]["x"], "number");
+  });
+
+   test.end();
 });
