2498a0e872a4447f6ce64a193c8a82bef3ce8efd
romsson <romain.vuillemot@gmail.com>
2017-02-04 22:12:55 +0100

    Diagonal layout as module and test

---
+++ src/grids/diagonal.js
@@ -0,0 +1,23 @@
+export default function(nodes, v) {
+
+  v.x.domain([0, nodes.length]).range([0, v.size[0]]);
+  v.y.domain([0, nodes.length]).range([0, v.size[1]]);
+
+  nodes.forEach(function(n, i) {
+
+    if(v.orient == "up") {
+      n[v.__x] = v.x(i) + v.offset[0];
+      n[v.__y] = v.size[1] - (v.y(i) + v.offset[1]) - v.size[1] / nodes.length;
+    } else {
+      n[v.__x] = v.x(i) + v.offset[0];
+      n[v.__y] = v.y(i) + v.offset[1];
+    }
+
+    n[v.__width] = v.size[0] / nodes.length;
+    n[v.__height] = v.size[1] / nodes.length;
+    n[v.__cx] = n[v.__x] + n[v.__width] / 2;
+    n[v.__cy] = n[v.__y] + n[v.__height] / 2;
+  });
+
+  return nodes;
+}
--- test/diagonal-test.js
+++ test/diagonal-test.js
@@ -13,3 +13,41 @@ tape("diagonal layout should return one central grid", function(test) {
   test.deepEqual(grid(nodes)[0]["height"], 20);
   test.end();
 });
+
+tape("diagonal with 2 elements splits the view in 2 equal parts", function(test) {
+
+  var nodes = ["A", "B"];
+
+  var grid = gridding.gridding().mode("diagonal").size([10, 20]);
+
+  test.deepEqual(grid(nodes)[0]["x"], 0);
+  test.deepEqual(grid(nodes)[0]["y"], 0);
+  test.deepEqual(grid(nodes)[0]["width"], 10 / 2);
+  test.deepEqual(grid(nodes)[0]["height"], 20 / 2);
+
+  test.deepEqual(grid(nodes)[1]["x"], 10 / 2);
+  test.deepEqual(grid(nodes)[1]["y"], 20 / 2);
+  test.deepEqual(grid(nodes)[1]["width"], 10 / 2);
+  test.deepEqual(grid(nodes)[1]["height"], 20 / 2);
+
+  test.end();
+});
+
+tape("diagonal offset", function(test) {
+
+  var nodes = ["A", "B"];
+
+  var grid = gridding.gridding().mode("diagonal").size([10, 20]).offset([15, 25]);
+
+  test.deepEqual(grid(nodes)[0]["x"], 15);
+  test.deepEqual(grid(nodes)[0]["y"], 25);
+  test.deepEqual(grid(nodes)[0]["width"], 10 / 2);
+  test.deepEqual(grid(nodes)[0]["height"], 20 / 2);
+
+  test.deepEqual(grid(nodes)[1]["x"], 15 +10 / 2);
+  test.deepEqual(grid(nodes)[1]["y"], 25 + 20 / 2);
+  test.deepEqual(grid(nodes)[1]["width"], 10 / 2);
+  test.deepEqual(grid(nodes)[1]["height"], 20 / 2);
+
+  test.end();
+});
