9789e9bb68f79ec6febaeadaabcc792f9cd5f071
romsson <romain.vuillemot@gmail.com>
2017-02-28 10:05:14 +0100

    Pyramid module

---
+++ test/pyramid-test.js
@@ -0,0 +1,15 @@
+var tape = require("tape"),
+    gridding = require("../");
+
+tape("cascade layout should return one central grid", function(test) {
+
+  var nodes = ["A"];
+
+  var grid = gridding.gridding().mode("pyramid").size([10, 20]);
+
+  test.deepEqual(grid(nodes)[0]["x"], 0);
+  test.deepEqual(grid(nodes)[0]["y"], 0);
+  test.deepEqual(grid(nodes)[0]["width"], 10);
+  test.deepEqual(grid(nodes)[0]["height"], 20);
+  test.end();
+});
