eecbaadca85e310739a2dc9fab9e681903f4400a
romsson <romain.vuillemot@gmail.com>
2017-02-28 10:04:58 +0100

    Grid module

---
+++ src/grids/grid.js
@@ -0,0 +1,34 @@
+export default function(nodes, v) {
+
+  v.cols = Math.ceil(Math.sqrt(nodes.length));
+  v.rows = Math.ceil(nodes.length / v.cols);
+
+  if(typeof v.cellSize !== "undefined" && v.cellSize !== null) {
+    v.size[0] = v.cellSize[0] * v.cols;
+    v.size[1] = v.cellSize[1] * v.rows;
+  }
+
+  v.x.domain([0, v.cols]).range([0, v.size[0]]);
+  v.y.domain([0, v.rows]).range([0, v.size[1]]);
+
+  nodes.forEach(function(n, i) {
+
+    var col = i % v.cols;
+    var row = Math.floor(i / v.cols);
+
+    n[v.__x] = v.x(col) + v.padding + v.offset[0];
+    n[v.__y] = v.y(row) + v.padding + v.offset[1];
+
+    n[v.__width] = v.size[0] / v.cols - 2 * v.padding;
+    n[v.__height] = v.size[1] / v.rows - 2 * v.padding;
+
+    if(v.orient == "up") {
+      n[v.__y] = v.size[1] - n[v.__y] - n[v.__height];
+    }
+
+    n[v.__cx] = n[v.__x] + n[v.__width] / 2;
+    n[v.__cy] = n[v.__y] + n[v.__height] / 2;
+  });
+
+  return nodes;
+}
--- test/grid-test.js
+++ test/grid-test.js
@@ -14,3 +14,27 @@ tape("grid layout if single should return full dimension", function(test) {
 
   test.end();
 });
+
+tape("grid layout with 2 elements", function(test) {
+
+  var nodes = [{}, {}];
+
+  var grid = gridding.gridding().mode("vertical").size([20, 10]);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [0, 10]);
+  test.deepEqual(nodes.map(function(d) { return d.y; }), [0, 0]);
+  test.end();
+});
+
+tape("grid layout with 3 elements", function(test) {
+
+  var nodes = [{}, {}, {}];
+
+  var grid = gridding.gridding().mode("vertical").size([30, 60]);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [0, 10, 20]);
+  test.deepEqual(nodes.map(function(d) { return d.y; }), [0, 0, 0]);
+  test.end();
+});
