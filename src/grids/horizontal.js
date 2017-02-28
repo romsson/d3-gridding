9d9d7cc1ab23b3279c54ef2869bf53012a5145fa
romsson <romain.vuillemot@gmail.com>
2017-02-28 10:05:07 +0100

    Horizontal module

--- src/grids/horizontal.js
+++ src/grids/horizontal.js
@@ -4,19 +4,19 @@ export default function(nodes, v) {
 
   v.rows = nodes.length;
 
-  v.y.domain([0, v.rows]).range([0, v.size[1]]);
+  v.y.domain([0, v.rows]).range([0, v.size[1] - 2 * v.padding]);
 
   var _valueY;
 
   if(!v.valueY) {
     _valueY = function() { return 1; }
-    v.y.domain([0, nodes.length]).range([0, v.size[1]]);
+    v.y.domain([0, nodes.length]).range([0, v.size[1] - 2 * v.padding]);
   } else {
     _valueY = v.valueY;
-    v.y.domain([0, d3Array.sum(nodes, _valueY)]).range([0, v.size[1]]);
+    v.y.domain([0, d3Array.sum(nodes, _valueY)]).range([0, v.size[1] - 2 * v.padding]);
   }
 
-  if(typeof v.cellSize !== "undefined") {
+  if(typeof v.cellSize !== "undefined" && v.cellSize !== null) {
     v.size[1] = v.cellSize[1] * v.rows;
   }
 
@@ -24,18 +24,17 @@ export default function(nodes, v) {
 
   if(!v.valueWidth) {
     _valueWidth = function() { return 1; }
-    v.width.domain([0, 1]).range([0, v.size[0]]);
+    v.width.domain([0, 1]).range([0, v.size[0] - 2 * v.padding]);
   } else {
     _valueWidth = v.valueWidth;
-    v.width.domain(d3Array.extent(nodes, _valueWidth)).range([0, v.size[0]]);
+    v.width.domain(d3Array.extent(nodes, _valueWidth)).range([0, v.size[0] - 2 * v.padding]);
   }
 
   nodes[0].y0 = 0;
 
   nodes.forEach(function(n, i) {
-
-    n[v.__x] = 0 + v.offset[0];
-    n[v.__y] = n.y0 + v.offset[1];
+    n[v.__x] = 0 + v.padding + v.offset[0];
+    n[v.__y] = n.y0 + v.padding + v.offset[1];
 
     n[v.__width] = v.width(_valueWidth(n));
     n[v.__height] = v.y(_valueY(n));
--- test/horizontal-test.js
+++ test/horizontal-test.js
@@ -24,3 +24,35 @@ tape("horizontal all elements should by default have identical height", function
   test.deepEqual(nodes.map(function(d) { return d.height === 20 / nodes.length; }), [true, true, true, true]);
   test.end();
 });
+
+
+tape("horizontal padding", function(test) {
+
+  var nodes = [{}, {}, {}, {}];
+
+  var grid = gridding.gridding().mode("horizontal").size([100, 200]).padding(10);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [10, 10, 10, 10]);
+//  test.deepEqual(nodes.map(function(d) { return d.y; }), [0 + 20, 50 + 20, 100 + 20, 150 + 20]);
+
+//  test.deepEqual(nodes.map(function(d) { return d.width === 100; }), [true, true, true, true]);
+//  test.deepEqual(nodes.map(function(d) { return d.height === 200 / nodes.length; }), [true, true, true, true]);
+  test.end();
+});
+
+
+tape("horizontal offset", function(test) {
+
+  var nodes = [{}, {}, {}, {}];
+
+  var grid = gridding.gridding().mode("horizontal").size([100, 200]).offset([10, 20]);
+  grid(nodes);
+
+  test.deepEqual(nodes.map(function(d) { return d.x; }), [10, 10, 10, 10]);
+  test.deepEqual(nodes.map(function(d) { return d.y; }), [0 + 20, 50 + 20, 100 + 20, 150 + 20]);
+
+  test.deepEqual(nodes.map(function(d) { return d.width === 100; }), [true, true, true, true]);
+  test.deepEqual(nodes.map(function(d) { return d.height === 200 / nodes.length; }), [true, true, true, true]);
+  test.end();
+});
