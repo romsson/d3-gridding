b6df58e39555c1f8865f7472b81a9fc900a0d202
romsson <romain.vuillemot@gmail.com>
2017-03-05 13:09:07 +0100

    Tree layout

---
+++ src/modes/tree.js
@@ -0,0 +1,34 @@
+import * as d3Hierarchy from "d3-hierarchy";
+
+export default function(nodes, v) {
+
+  var treeData = d3Hierarchy.stratify()
+    .id(function(d, i) { return i; })
+    .parentId(function(d, i) {
+      return i === 0 ? null: 0;
+    })([{}].concat(nodes));
+
+  var tree = d3Hierarchy.tree()
+      .size([v.size[0], v.size[1] / 2])
+
+  var treeLayout = d3Hierarchy.hierarchy(treeData, function(d) {
+      return d.children;
+    });
+
+  treeLayout = tree(treeLayout);
+
+  nodes.forEach(function(n, i) {
+
+    n[v.__width] = v.cellSize ? v.cellSize[0]: v.size[0] / nodes.length;
+    n[v.__height] = v.cellSize ? v.cellSize[1]: v.size[1] / nodes.length;
+
+    n[v.__x] = treeLayout.children[i].x + v.offset[0] - n[v.__width] / 2;
+    n[v.__y] = treeLayout.children[i].y + v.offset[1] - n[v.__height] / 2;
+
+    n[v.__cx] = n[v.__x] + n[v.__width] / 2;
+    n[v.__cy] = n[v.__y] + n[v.__height] / 2;
+
+  });
+
+  return nodes;
+}
