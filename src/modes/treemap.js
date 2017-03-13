3b364b3604aac9a8ba5f53bfb9ef0814847ba671
romsson <romain.vuillemot@gmail.com>
2017-03-13 12:48:14 +0100

    Cleaning

--- src/modes/treemap.js
+++ src/modes/treemap.js
@@ -7,24 +7,25 @@ export default function(nodes, v) {
       .padding(v.padding);
 
   var stratify = d3Hierarchy.stratify()
-      .id(function(d, i) { return i; })
-      .parentId(function(d, i) {
-        return i === 0 ? "": 0;
-      });
+      .id(v.id)
+      .parentId(v.parentId);
 
-  var r = stratify([{}].concat(nodes)).sum(function() { return 1; });
+  var root = stratify([{}].concat(nodes))
+      .sum(function() { return 1; });
 
-  var tree = treemap(r);
+  var tree = treemap(root);
 
   nodes.forEach(function(n, i) {
-    n[v.__x] = tree.children[i].x0 + v.offset[0];
-    n[v.__y] = tree.children[i].y0 + v.offset[1];
 
-    n[v.__width] = tree.children[i].x1 - tree.children[i].x0;
-    n[v.__height] = tree.children[i].y1 - tree.children[i].y0;
+    n[v.__x] = tree.descendants()[i].x0 + v.offset[0];
+    n[v.__y] = tree.descendants()[i].y0 + v.offset[1];
+
+    n[v.__width] = tree.descendants()[i].x1 - tree.descendants()[i].x0;
+    n[v.__height] = tree.descendants()[i].y1 - tree.descendants()[i].y0;
 
     n[v.__cx] = n[v.__x] + n[v.__width] / 2;
     n[v.__cy] = n[v.__y] + n[v.__height] / 2;
+
   });
 
   return nodes;
