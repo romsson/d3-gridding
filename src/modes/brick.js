f182582f4e1d959f4c4a54974151ee3e8e5927c7
romsson <romain.vuillemot@gmail.com>
2017-04-28 13:38:50 +0200

    Now using shiftX/Y for offset and in all directions

--- src/modes/brick.js
+++ src/modes/brick.js
@@ -1,13 +1,18 @@
 export default function(nodes, v) {
 
-  var shiftX = 1/2;
-  var shiftY = 1/2;
+  if(!v.shiftX) {
+    v.shiftX = 1/2;
+  }
+
+  if(!v.shiftY) {
+    v.shiftY = 1/2;
+  }
 
   v.cols = Math.ceil(Math.sqrt(nodes.length));
   v.rows = Math.ceil(nodes.length / v.cols);
 
-  v.x.domain([0, v.cols + shiftX]).range([0, v.size[0]]);
-  v.y.domain([0, v.rows + shiftY]).range([0, v.size[1]]);
+  v.x.domain([0, v.cols]).range([0, v.size[0] - v.size[0] / v.cols]);
+  v.y.domain([0, v.rows]).range([0, v.size[1]]);
 
   nodes.forEach(function(n, i) {
 
@@ -17,16 +22,31 @@ export default function(nodes, v) {
     n[v.__x] = v.x(col) + v.offset[0] + v.padding;
     n[v.__y] = v.y(row) + v.offset[1] + v.padding;
 
-    n[v.__width] = v.size[0] / (v.cols + shiftX) - 2 * v.padding;
-    n[v.__height] = v.size[1] / (v.rows + shiftY) - 2 * v.padding;
+    n[v.__width] = v.x.range()[1] / v.cols - 2 * v.padding;
+    n[v.__height] = v.y.range()[1] / v.rows - 2 * v.padding;
 
     if(v.orient === "left") {
       if(row % 2 === 1) {
-        n[v.__x] += n[v.__width] / 2;
+        n[v.__x] += n[v.__width] * v.shiftX;
       }
+    } else if(v.orient === "up") {
+      if(col % 2 === 1) {
+        n[v.__y] += n[v.__height] * v.shiftY;
+      }
+    } else if(v.orient === "down") {
+      if(col % 2 === 0) {
+        n[v.__y] += n[v.__height] * v.shiftY;
+      }
+    } else if(v.orient === "right") {
+      if(row % 2 === 0) {
+        n[v.__x] += n[v.__width] * v.shiftX;
+      }
+    } else if(v.orient === "none") {
+      n[v.__x] += 0;
+      n[v.__y] += 0;
     } else { // default right
       if(row % 2 === 0) {
-        n[v.__x] += n[v.__width] / 2;
+        n[v.__x] += n[v.__width] * v.shiftX;
       }
     }
 
