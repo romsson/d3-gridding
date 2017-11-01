baedf96ce1254c70f09ce3c76b444fa133ab2838
romsson <romain.vuillemot@gmail.com>
2017-11-01 15:24:19 +0100

    Don't override the vars.cols/vars.rows if not set

--- src/modes/grid.js
+++ src/modes/grid.js
@@ -1,16 +1,24 @@
 export default function(nodes, v) {
 
+  var _cols;
+
   if(!v.cols) {
-    v.cols = Math.ceil(Math.sqrt(nodes.length));
+    _cols = Math.ceil(Math.sqrt(nodes.length));
+  } else {
+    _cols = v.cols;
   }
 
+  var _rows;
+
   if(!v.rows) {
-    v.rows = Math.ceil(nodes.length / v.cols);
+    _rows = Math.ceil(nodes.length / _cols);
+  } else {
+    _rows = v.rows;
   }
 
   if(v.cellSize) {
-    v.size[0] = v.cellSize[0] * v.cols;
-    v.size[1] = v.cellSize[1] * v.rows;
+    v.size[0] = v.cellSize[0] * _cols;
+    v.size[1] = v.cellSize[1] * _rows;
   }
 
   // var _valueWidth = function() { return 1; }
@@ -19,13 +27,13 @@ export default function(nodes, v) {
   // var _valueHeight = function() { return 1; }
   v.height.domain([0, 1]).range([0, v.size[1] - 2 * v.padding]);
 
-  v.x.domain([0, v.cols]).range([0, v.size[0]]);
-  v.y.domain([0, v.rows]).range([0, v.size[1]]);
+  v.x.domain([0, _cols]).range([0, v.size[0]]);
+  v.y.domain([0, _rows]).range([0, v.size[1]]);
 
   nodes.forEach(function(n, i) {
 
-    var col = i % v.cols;
-    var row = Math.floor(i / v.cols);
+    var col = i % _cols;
+    var row = Math.floor(i / _cols);
 
     n[v.__x] = v.x(col) + v.offset[0] + v.padding;
     n[v.__y] = v.y(row) + v.offset[1] + v.padding;
@@ -33,8 +41,8 @@ export default function(nodes, v) {
     // n[v.__width] = v.width(_valueWidth(n));
     // n[v.__height] = v.height(_valueHeight(n));
 
-    n[v.__width] = v.size[0] / v.cols - 2 * v.padding;
-    n[v.__height] = v.size[1] / v.rows - 2 * v.padding;
+    n[v.__width] = v.size[0] / _cols - 2 * v.padding;
+    n[v.__height] = v.size[1] / _rows - 2 * v.padding;
 
     if(v.orient == "up") {
       n[v.__y] = v.size[1] - n[v.__y] - n[v.__height];
