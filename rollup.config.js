8e0e88b9e8910134b53c8642e96bac0a3982d66f
Philippe Rivière <fil@rezo.net>
2019-12-03 12:50:16 +0100

    upgrade & modernize dependencies & toolchain

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,36 @@
-import npm from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
 
-export default {
-  entry: 'index.js',
-  plugins: [npm({jsnext: true}), commonjs({})],
-  moduleId: 'd3-gridding',
-  moduleName: 'd3-gridding',
-  dest: 'd3-gridding.js',
-  format: 'umd'
+const config = {
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  output: {
+    file: `dist/${meta.name}.js`,
+    name: "d3",
+    format: "umd",
+    indent: false,
+    extend: true,
+    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+  },
+  plugins: []
 };
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: `dist/${meta.name}.min.js`
+    },
+    plugins: [
+      ...config.plugins,
+      terser({
+        output: {
+          preamble: config.output.banner
+        }
+      })
+    ]
+  }
+];
